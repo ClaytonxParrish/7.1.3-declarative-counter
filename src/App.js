e2875ab7ae1f2afa8196c9442de4361c5608fb94
ClaytonxParrish <105694697+ClaytonxParrish@users.noreply.github.com>
2023-05-17 13:39:48 -0700

    add some css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let [counter, changeCounter] = useState(1);
-window.changeCounter = changeCounter;
-
+   let [counter, changeCounter] = useState(1);
+   window.changeCounter = changeCounter;
   return (
-    <div>
-      <h1>
-        Counter
-      </h1>
-      <p>
-        {counter}
-      </p>
-      <p>
-        {counter}
-      </p>
+    <div className="box">
+    <div className="container">
+      <h1 className="title">Counter</h1>
+      <p className="counter">{counter}</p>
+      <p className="counter">{counter}</p>
+    </div>
     </div>
   );
 }
